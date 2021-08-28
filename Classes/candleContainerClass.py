f68d569efeee24bb3df32873b2504866aaf5b22d
ian <iacosta3994@gmail.com>
2021-08-27 20:25:15 -0400

    Specifies price of candle and containers needed to complete

--- Classes/candleContainerClass.py
+++ Classes/candleContainerClass.py
@@ -5,6 +5,18 @@ class candleContainerClass:
         self.price = price
     
     def fillContainers (self,totalWeight):
-        self.weightPerContainer = totalWeight / self.quantity
-        self.MaxVolumeInGrams = self.maxVolume * 29.5735296875
-        self.filledContainers = 
+        self.maxGramsInContainer = self.maxVolume * 29.5735296875
+        if self.maxGramsInContainer > totalWeight/self.quantity:
+            print("Container max Volume: "+str(self.maxGramsInContainer))
+            print("Total Weight per container: " + str(totalWeight/self.quantity))
+            print("Containers needed: "+ str(totalWeight/self.maxGramsInContainer) + '\n' )
+            return totalWeight/self.maxGramsInContainer
+            
+        elif self.maxGramsInContainer < totalWeight/self.quantity:
+            print("Container max Volume: "+str(self.maxGramsInContainer))
+            print("Total Weight per container: " + str(totalWeight/self.quantity))
+            print("Containers needed: "+ str(totalWeight/self.maxGramsInContainer)+ '\n' )
+            return totalWeight/self.maxGramsInContainer
+        else:
+            print("Error for FillContainers in Candle CandleContainerClass")
+            return None
